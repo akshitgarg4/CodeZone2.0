c88296e44bf44056589ba203a85de68a96f9dc58
Gaganpreet Khurana <54024846+gaganpreetkhurana@users.noreply.github.com>
2022-03-31 09:53:23 +0530

    Login issue fixed

--- backend/config/authenticate.js
+++ backend/config/authenticate.js
@@ -1,20 +1,21 @@
 const jwt = require ( "jsonwebtoken" );
 
 module.exports.authenticateToken = function ( req , res , next ) {
-    const authHeader = req.headers[ "authorization" ];
-    const token = authHeader && authHeader.split ( " " )[ 1 ];
-    if ( token == null ) {
-        return res.json ( 422 , {
-            message : "Server Error token not found" ,
-        } );
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(token == null){
+        return res.json(422, {
+            message: "Server Error token not found",
+        });
     }
-    jwt.verify ( token , "CODEZONE2.0" , ( err , user ) => {
-        if ( err ) {
-            return res.json ( 422 , {
-                message : "Server Error while retrieving token" ,
-            } );
+    jwt.verify(token, "CODEZONE2", (err, user) => {
+        console.log(token, err, user, authHeader);
+        if(err){
+            return res.json(422, {
+                message: "Server Error while retrieving token",
+            });
         }
         req.user = user;
-        next ();
-    } );
+        next();
+    });
 };
