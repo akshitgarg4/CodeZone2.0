fbf59d285f21c6e716f6f6e2c0a98c7b9ef68b97
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-04-26 19:19:30 +0530

    Evaluators are the ones mapped. Fetch marks edited Fetch marks data will now contain 2 objects

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
+const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     picture: { type: String },
-  },
-  { timestamps: true }
-);
+    evaluator: { type: Boolean, required: true, default: false }
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
