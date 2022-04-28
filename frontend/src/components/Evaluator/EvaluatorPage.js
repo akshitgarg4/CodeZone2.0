67fa12f4dd9d102a1fdb9610397df1c862601313
Akshit Garg <54847659+akshitgarg4@users.noreply.github.com>
2022-04-28 14:24:13 +0530

    Evaluator complete data fetched

--- frontend/src/components/Evaluator/EvaluatorPage.js
+++ frontend/src/components/Evaluator/EvaluatorPage.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function EvaluatorPage() {
+    const {recordId} = useParams();
+    const [description,setDescription] = useState('');
+    const [data,setData] = useState([]);
+
+    // useEffect(() => {
+	// 	//Need to fetch complete data of the class which evaluator has to fill
+	// 	console.log(recordId);
+	// 	fetch(`/data/evaluator/marksFetch/${recordId}`, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${localStorage.getItem('CodeZone2_Token')}`
+	// 		}
+	// 	})
+	// 		.then((response) => response.json())
+	// 		.then((data) => {
+	// 			if(data?.success){
+    //                 setDescription(data?.description);
+    //                 setData(data?.data);
+	// 			}
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err)
+	// 		});
+	// }, [recordId])
   return (
     <div>EvaluatorPage</div>
   )
