9090604cff51db9f23970deb5e506f0e8c48ba31
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-04-30 20:27:20 +0530

    Minor Fixes, Evaluator page working now

--- backend/addTeachers.js
+++ backend/addTeachers.js
@@ -1,42 +1,42 @@
 const User = require("./models/user")
 
 const teachers = {
-    "be18103032 Gaganpreet Singh Khurana": "gaganpreetsinghkhurana.be18cse@pec.edu.in",
-    "Akshit Garg": "gargakshit.be18cse@pec.edu.in",
+	"be18103032 Gaganpreet Singh Khurana": "gaganpreetsinghkhurana.be18cse@pec.edu.in",
+	"Akshit Garg": "gargakshit.be18cse@pec.edu.in",
 }
 
-module.exports.createUsers = async function() {
-    let newTeachers = []
-    for (const [teacherName, teacherEmail] of Object.entries(teachers)) {
-        let teacherDocument = await User.findOne({ "email": teacherEmail.toLowerCase() });
-        if (!teacherDocument) {
-            teacherDocument = await User.create({
-                "name": teacherName,
-                "email": teacherEmail.toLowerCase(),
-                "picture": "",
-                "evaluator": true,
-            })
-            teacherDocument = await teacherDocument.save();
-            newTeachers.push([teacherName, teacherEmail.toLowerCase()])
-        }
-    }
-    console.log("New Teachers", newTeachers);
-
-    let allUsers = await User.find();
-    for (let userIndex = 0; userIndex < allUsers.length; userIndex++) {
-        if (allUsers[userIndex].name in teachers) {
-            allUsers[userIndex].evaluator = true;
-        } else {
-            allUsers[userIndex].evaluator = false;
-        }
-
-        await allUsers[userIndex].save();
-
-    }
-    console.log("Evaluators: ", await User.find({
-            evaluator: true
-        })
-        .select(
-            "name email"
-        ))
+module.exports.createUsers = async function(){
+	let newTeachers = []
+	for(const [teacherName, teacherEmail] of Object.entries(teachers)){
+		let teacherDocument = await User.findOne({"email": teacherEmail.toLowerCase()});
+		if( !teacherDocument){
+			teacherDocument = await User.create({
+				"name": teacherName,
+				"email": teacherEmail.toLowerCase(),
+				"picture": "",
+				"evaluator": true,
+			})
+			teacherDocument = await teacherDocument.save();
+			newTeachers.push([teacherName, teacherEmail.toLowerCase()])
+		}
+	}
+	console.log("New Teachers", newTeachers);
+	
+	let allUsers = await User.find();
+	for(let userIndex = 0; userIndex < allUsers.length; userIndex++){
+		if(allUsers[userIndex].name in teachers){
+			allUsers[userIndex].evaluator = true;
+		} else{
+			allUsers[userIndex].evaluator = false;
+		}
+		
+		await allUsers[userIndex].save();
+		
+	}
+	console.log("Evaluators: ", await User.find({
+			evaluator: true
+		})
+		.select(
+			"-_id name email"
+		))
 }
--- frontend/src/components/Evaluator/AddMarks.js
+++ frontend/src/components/Evaluator/AddMarks.js
@@ -3,18 +3,18 @@ import {Grid} from "@mui/material";
 import Sheet from "./Sheet";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
 
-export default function AddMarks() {
-    const location = useLocation();
-    const { id, recordId } = location?.state;
-    const [groupNumber, setGroupNumber] = useState(id);
+export default function AddMarks(){
+	const location = useLocation();
+	const {id, recordId} = location?.state;
+	const [groupNumber, setGroupNumber] = useState(id);
 	const [groupID, setGroupID] = useState("");
 	const [dataFetched, setDataFetched] = useState(false);
 	const [studentsData, setStudentsData] = useState([]);
-
-    useEffect(() => {
+	
+	useEffect(() => {
 		if(groupNumber !== 0 && studentsData.length > 0 && groupID !== ""){
 			// console.log(studentsData, "EEE")
 			setDataFetched(true);
@@ -23,27 +23,27 @@ export default function AddMarks() {
 			setDataFetched(false);
 		}
 	}, [groupNumber, studentsData, groupID])
-    useEffect(() => {
-        // api to get the list of the group with existing marks by GroupNumber
-        // console.log(groupNumber);
-        fetch(`/data/evaluator/record_number/${recordId}/group_number/${groupNumber}`, {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Bearer ${localStorage.getItem("CodeZone2_Token")}`,
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-              
-            if (data?.success) {
-                setGroupNumber(data.data[0].GroupNumber);
-				setGroupID(data.data[0].groupID);
-				setStudentsData(data.data[0].students);
-            }
-          });
-      }, [id,groupNumber,recordId]);
-  return (
-    <Grid container>
+	useEffect(() => {
+		// api to get the list of the group with existing marks by GroupNumber
+		// console.log(groupNumber);
+		fetch(`/data/evaluator/record_number/${recordId}/group_number/${groupNumber}`, {
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Authorization: `Bearer ${localStorage.getItem("CodeZone2_Token")}`,
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				
+				if(data?.success){
+					setGroupNumber(data.data[0].GroupNumber);
+					setGroupID(data.data[0].groupID);
+					setStudentsData(data.data[0].students);
+				}
+			});
+	}, [id, groupNumber, recordId]);
+	return (
+		<Grid container>
 			<Box
 				m={3}
 				sx={{
@@ -53,8 +53,9 @@ export default function AddMarks() {
 			>
 				<TextField fullWidth label="Group No" id="fullWidth" defaultValue={groupNumber}/>
 			</Box>
-			{dataFetched && <Sheet studentsData={studentsData} groupNumber={groupNumber} groupID={groupID} recordID={recordId}/>}
+			{dataFetched &&
+				<Sheet studentsData={studentsData} groupNumber={groupNumber} groupID={groupID} recordID={recordId}/>}
 		</Grid>
-  )
+	)
 }
 
