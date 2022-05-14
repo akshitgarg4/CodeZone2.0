f9f9fe2a6f4c8a6face31da6dc80ee836f15177d
Akshit Garg <54847659+akshitgarg4@users.noreply.github.com>
2022-05-14 13:13:14 +0530

    Auth token reset

--- backend/config/credentials.js
+++ backend/config/credentials.js
@@ -1,14 +1,17 @@
 module.exports.SECRET = {
-	CLIENT_ID: '626526940054-1l3u3mkcp8pd34v695mi4gt2f5k79hfi.apps.googleusercontent.com',
-	CLIENT_SECRET_KEY: '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',
+  CLIENT_ID:
+    "626526940054-1l3u3mkcp8pd34v695mi4gt2f5k79hfi.apps.googleusercontent.com",
+  CLIENT_SECRET_KEY: "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",
 };
 
 module.exports.SECRET_MAIL = {
-	CLIENT_ID: '251136267954-40vk8lqb8pe3vqjbeap4ors2ung1g85j.apps.googleusercontent.com',
-	CLIENT_SECRET_KEY: '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',
-	EMAIL: 'codezonemajorproject@gmail.com',
-	FROM: "no-reply@codezone.com",
-	ACCESS_TOKEN: 'ya29.A0ARrdaM-jU3P-n9WkwhOs-W0ZZSejLoaNdBJxAPR3SgvW1EGTUNCAYvDyDt6tFNY_Ni0WNdVGIIH9h6ZgJhbY8yCgsgSV3G_5pY2fnijM0G4mQiaMHRq-TAa0PPXIQSRZLSe7ceblHfwjzfmRwku43pnvfVWj',
-	REFRESH_TOKEN: '1//04I6L722egyhSCgYIARAAGAQSNwF-L9IrzfuNSzuLBvE4oJnM4bOO2-rxwrK2U66oyP3bEaAaQWY-qBUidwRq6btQJLXEUvjVJcI',
-}
-
+  CLIENT_ID:
+    "251136267954-40vk8lqb8pe3vqjbeap4ors2ung1g85j.apps.googleusercontent.com",
+  CLIENT_SECRET_KEY: "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",
+  EMAIL: "codezonemajorproject@gmail.com",
+  FROM: "no-reply@codezone.com",
+  ACCESS_TOKEN:
+    "ya29.A0ARrdaM-r_hPuZ4ifsM4EDw7lKLTfaVWDp0nPRt3OTBYLbhOHNkJtc9SuS1nU3pHl1WXosVELMKLKa2wB-FFnB_1Kc7wifAJ33A4cQGuOVBAcirGzYXDUbJklRuqvTgXSdLxo80mVvGqm_sMdAkGp8pNiG3cD",
+  REFRESH_TOKEN:
+    "1//04852jf_PojmiCgYIARAAGAQSNwF-L9Ir5Hz08C3OMCNoCb1JKWroSsUMyDTiUGgT8Scsji23gJwIcasjPqFfB461Hzj7yp00nt8",
+};
