580b052dc115cfd20cad184d914b97732642d743
Akshit Garg <54847659+akshitgarg4@users.noreply.github.com>
2022-06-29 19:46:53 +0530

    Final Commits

--- backend/config/credentials.js
+++ backend/config/credentials.js
@@ -1,14 +1,13 @@
 module.exports.SECRET = {
-	CLIENT_ID: '626526940054-1l3u3mkcp8pd34v695mi4gt2f5k79hfi.apps.googleusercontent.com',
-	CLIENT_SECRET_KEY: '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',
+  CLIENT_ID: "",
+  CLIENT_SECRET_KEY: "",
 };
 
 module.exports.SECRET_MAIL = {
-	CLIENT_ID: '251136267954-40vk8lqb8pe3vqjbeap4ors2ung1g85j.apps.googleusercontent.com',
-	CLIENT_SECRET_KEY: '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',
-	EMAIL: 'codezonemajorproject@gmail.com',
-	FROM: "no-reply@codezone.com",
-	ACCESS_TOKEN: 'ya29.A0ARrdaM_vWVUbmGH7x9WOTeD5c6fp4d5h7Px8cafQpXwTKno8a8cKXst6lgWVS4oNzcLKwGRE4LqqKK0Pw63cIdfj7ICZ120OJ0QO2ylO27lWpZh57Dm8R4aePHR52djBx2SwxLi71KP0Cz12CFGhrgfW9HHj',
-	REFRESH_TOKEN: '1//04bLoQMC5fTseCgYIARAAGAQSNwF-L9Ir-W0ud2UstBl3GDbdEo6ciJOOUCHHF7zXTb0T7R26VPL760vFV-J-_RdI0vIzEQpM90M',
-}
-
+  CLIENT_ID: "",
+  CLIENT_SECRET_KEY: "",
+  EMAIL: "codezonemajorproject@gmail.com",
+  FROM: "no-reply@codezone.com",
+  ACCESS_TOKEN: "",
+  REFRESH_TOKEN: "",
+};
--- frontend/src/credentials/credentials.js
+++ frontend/src/credentials/credentials.js
@@ -1,5 +1,5 @@
 const SECRET = {
-    CLIENT_ID : '626526940054-1l3u3mkcp8pd34v695mi4gt2f5k79hfi.apps.googleusercontent.com' ,
-    CLIENT_SECRET_KEY : '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'
-}
-module.exports = SECRET
+  CLIENT_ID: "",
+  CLIENT_SECRET_KEY: "",
+};
+module.exports = SECRET;
